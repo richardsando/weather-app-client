6a1bcd18719b183f38e02373073d28397ec56ab4
Richard Sando <richardsando369@gmail.com>
2019-11-26 21:36:26 +1100

    fixed links

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className="footer">
-            <a href="github.com/richardsando">github.com/richardsando</a>
+            <a href="https://github.com/richardsando">github.com/richardsando</a>
             <a href="https://darksky.net/forecast">darksky.net</a>
         </div>
     )
